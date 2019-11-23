9502420957036cb0311c7d13c4717022944e6398
modoue <modou2015diop@gmail.com>
2019-11-23 15:04:05 +0100

    first

---
+++ src/main/java/calcul/Calcul.java
@@ -0,0 +1,16 @@
+package calcul;
+
+public class Calcul {
+	public Calcul()
+	{
+		
+	}
+	public double addition(double a,double b)
+	{
+		return a+b;
+	}
+public double multiplication(double a,double b)
+{
+	return a*b;
+}
+}
